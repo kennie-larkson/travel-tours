849a56eed9468dfde363594167a3093b6032247e
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-12-29 11:22:32 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## React Projects Starter APP
+
+# To run this application
+
+1. First clone the repo to a favorable location on your machine
+2. Open your terminal and navigate to the directory where your cloned the project
+3. CD into the project folder
+4. Install project dependencies by running the command: npm install
+5. After installing the dependencies you can start the application with the command: npm start and your browser should come up after some time
--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const data = response.json();
+
+      return setTours(data);
+    } catch (error) {
+      throw new Error(`oops!: ${error}`);
+    }
+  };
+  console.log(tours);
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  return <main>Tours Project Setup</main>;
 }
 
-export default App
+export default App;
