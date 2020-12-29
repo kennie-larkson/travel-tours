046d5406c09c4e6243643ff4bb2941accdd79ff5
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-12-29 13:38:08 +0100

    app completed with all intended functionality

--- src/Tour.js
+++ src/Tour.js
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, name, info, image, price }) => {
+const Tour = ({ id, name, info, image, price, handleDelete }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
-      <img src={image} />
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? `${info}` : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => handleDelete(id)}>not interested</button>
+      </footer>
+      
     </article>
   );
 };
